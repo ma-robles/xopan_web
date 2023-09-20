fe3da86b971011a038d6a4ec2eed3ca9a3af8fc7
miguel robles <miguel.robles@atmosfera.unam.mx>
2023-09-20 11:46:28 -0600

    versión inicial, formato básico

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 //sel code
 //
-const btn_upload = document.getElementById('btn_upload');
-btn_upload.addEventListener("click", selID);
+const sel_plantel= document.getElementById('plantel');
+sel_plantel.addEventListener("change", selID);
 function selID(){
     ID= document.getElementById('plantel').value;
     alert("Se mostrarán los datos de "+ID);
     plot(ID);
 }
 
-//send code
-const now= new Date().toISOString().split('Z')[0];
-document.getElementById("datetime").value = now;
-btn_send = document.getElementById("btn_send");
-btn_send.addEventListener('click', function(){
-    document.getElementById('form_upload').requestSubmit();
-    window.alert('Los datos serán  enviados');
-
-});
